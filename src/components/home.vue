bc91922e6a49ed3ad57ccb5f5d981064ae81eecf
duanyue <171193194@qq.com>
2022-05-03 19:21:29 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const map = ref(null)
+onMounted(()=>{
+console.log('leaflet::',__MAP__)
+const dom = document.getElementById('map')
+__MAP__.init(dom)
+console.log('map::',map)
+})
+
+
+</script>
+
+<template>
+  <div ref="map" id="map">你好</div>
+</template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+a {
+  color: #42b983;
+}
+</style>
